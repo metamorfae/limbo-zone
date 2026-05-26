a4ffa0e0371c8b439a744dd93330bbb04afb763f
metamorfae <3va004@gmail.com>
2026-05-26 19:24:09 +0800

    Revert "Layout"
    This reverts commit 1b551944ffce473e175f28bc411f58a96a6f75b0.

--- limbo-zone.nekoweb.org/navbarCollapse.js
+++
@@ -1,8 +0,0 @@
-function navbarCollapse() {
-  var x = document.getElementById("navbar");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-} 
